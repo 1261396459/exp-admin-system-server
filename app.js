a247a57f00e503b228e77c56efd455cc8b5d4820
alsoeasy <1261396459@qq.com>
2021-08-03 23:14:35 +0800

    添加依赖

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());//跨域
+app.use(express.urlencoded({ extended: false }))//获取post body
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
