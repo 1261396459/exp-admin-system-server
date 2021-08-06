4c05a95c9f98335cb15d6d9412187a95b798c28c
alsoeasy <1261396459@qq.com>
2021-08-06 22:03:14 +0800

    修改put delete获取参数方式

--- routes/lab_adminer.js
+++ routes/lab_adminer.js
@@ -7,24 +7,58 @@ router.get('/my-labs', function (req, res, next) {
     let tid = req.query.uid;
     console.log(tid);
     database.Retrieve(`
-        SELECT Labinfor.LNAME,SPN,SPCs,SuIDs
+        SELECT Labinfor.LNAME,SPN,SPCs
         FROM Labinfor
         LEFT JOIN(
-            SELECT LNAME,GROUP_CONCAT(SPC)AS SPCs,GROUP_CONCAT(ID)AS SuIDs
+            SELECT LNAME,GROUP_CONCAT(CNAME,'(',SPC,')')AS SPCs
             FROM Supports
+            LEFT JOIN Couinfor ON Couinfor.CID=Supports.SPC
             GROUP BY LNAME
         )AS Temp ON Temp.LNAME=Labinfor.LNAME
         WHERE AMTID=?;
-  `,
+    `,
     [tid], (result) => {
+        res.send(result);
+    });
+});
+
+// 查看支持的课程
+router.get('/lab-sup', function (req, res, next) {
+    let lname = req.query.lname;
+    console.log(lname);
+    database.Retrieve(`
+        SELECT SPC AS CID,CNAME
+        FROM Supports
+        LEFT JOIN Couinfor ON Couinfor.CID=Supports.SPC
+        WHERE LNAME=?;
+  `,
+    [lname], (result) => {
+        res.send(result);
+    });
+});
+
+// 查看不支持的课程
+router.get('/lab-nsup', function (req, res, next) {
+    let lname = req.query.lname;
+    console.log(lname);
+    database.Retrieve(`
+        SELECT CID,CNAME
+        FROM Couinfor
+        WHERE CID NOT IN(
+            SELECT SPC
+            FROM Supports
+            WHERE LNAME=?
+        );
+  `,
+    [lname], (result) => {
         res.send(result);
     });
 });
 
 // 修改实验室容纳人数
 router.put('/upd-lab-number', function (req, res, next) {
-    let number = req.query.number;
-    let lab = req.query.lab;
+    let number = req.body.number;
+    let lab = req.body.lab;
     console.log(number,lab);
     database.Update(`
         UPDATE Labinfor
@@ -53,8 +87,8 @@ router.post('/add-lab-course', function (req, res, next) {
 
 // 删除实验室支持课程
 router.delete('/del-lab-course', function (req, res, next) {
-    let eid = req.query.eid;
-    let lab = req.query.lab;
+    let eid = req.body.eid;
+    let lab = req.body.lab;
     console.log(eid,lab);
     database.Delete(`
         DELETE FROM Supports
@@ -70,9 +104,11 @@ router.get('/application', function (req, res, next) {
     let tid = req.query.uid;
     console.log(tid);
     database.Retrieve(`
-        SELECT APID,LNAME,SPN,EID,APTIME
+        SELECT Application.*,CNAME,SPN,ExpNum.CPNUM
         FROM Application
-        NATURAL JOIN Labinfor
+        LEFT JOIN Labinfor ON Labinfor.LNAME=Application.LNAME
+        LEFT JOIN Couinfor ON Couinfor.CID=Application.EID
+        LEFT JOIN \`实验课人数表\`AS ExpNum ON ExpNum.EID=Application.EID
         WHERE Labinfor.AMTID=?;
     `,
     [tid], (result) => {
@@ -82,7 +118,7 @@ router.get('/application', function (req, res, next) {
 
 // 同意申请
 router.put('/agree-application', function (req, res, next) {
-    let apid = req.query.apid;
+    let apid = req.body.apid;
     console.log(apid);
     database.Update(`
         UPDATE application
@@ -96,8 +132,8 @@ router.put('/agree-application', function (req, res, next) {
 
 // 拒绝申请
 router.put('/disagree-application', function (req, res, next) {
-    let apid = req.query.apid;
-    let comment = req.query.comment;
+    let apid = req.body.apid;
+    let comment = req.body.comment;
     console.log(apid,comment);
     database.Update(`
         UPDATE application
